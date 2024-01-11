90cd832da6de0dcd5e21f02e4fc650317ab8bba8
Piimka <brandon.valery301222@gmail.com>
2024-01-11 16:43:56 +0100

    modifié

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ function showEmailAdress() {
 }
 
 function downloadCV() {
-    var cvURL = "CV.png";
-    var cvName = "CV.png";
+    var cvURL = "CV.jpg";
+    var cvName = "CV.jpg";
 
     var link = document.createElement("a");
     link.href = cvURL;
